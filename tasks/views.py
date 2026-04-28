e4403c8ce726de0a3a053196dc6065ec15c2e7ea
Kevin Plourdeau <kevin.plourdeau@gmail.com>
2026-04-28 21:45:24 +0200

    Sprint 3 - Add task creation form

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def task_list(request):
@@ -7,4 +8,19 @@ def task_list(request):
 
     return render(request, "tasks/task_list.html", {
         "tasks": tasks
+    })
+
+
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm()
+
+    return render(request, "tasks/task_create.html", {
+        "form": form
     })
